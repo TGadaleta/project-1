9ab0acba83162257891a8cfb316a674386ee7562
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-10-25 14:50:55 -0400

    restart and doing it properly

--- script.js
+++ script.js
@@ -11,23 +11,23 @@ const startBtn = document.querySelector('#startGameButton')
 const gridSize = 10 //how man cell rows and cols on each board 
 message = display.textContent = 'Press Start Game'
 
-function init(){
-    createBoard(playerBoard) //creates player board
-    createBoard(computerBoard) //creates computer board
-}
+// function init(){
+//     createBoard(playerBoard) //creates player board
+//     createBoard(computerBoard) //creates computer board
+// }
 
 
 //Create Boards Function
-function createBoard(boardEl){
-    for (let row = 0;row < gridSize;row++){
-        for (let col = 0;col < gridSize;col++){
-            let div = document.createElement('div')
-            div.dataset.row = row  //data attributs for finding location of cell
-            div.dataset.col = col
-            boardEl.appendChild(div)
-        }
-    }
-}
+// function createBoard(boardEl){
+//     for (let row = 0;row < gridSize;row++){
+//         for (let col = 0;col < gridSize;col++){
+//             let div = document.createElement('div')
+//             div.dataset.row = row  //data attributs for finding location of cell
+//             div.dataset.col = col
+//             boardEl.appendChild(div)
+//         }
+//     }
+// }
     
 //Create Score sheets
 //Fill out scoresheets
